62a14981fb32b5556cc486c5663fc88307eceaf4
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2020-11-17 13:53:52 +0530

    Create arithmetic.go

---
+++ arithmetic.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x,y:=5,6		
+	fmt.Println("y + y = ",x+y)
+	fmt.Println("y - y = ",x-y)
+	fmt.Println("y * y = ",x*y)
+	fmt.Println("y / y = ",x/y)
+	fmt.Println("x mod y = ",x%y)
+
+}
