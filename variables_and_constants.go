93ced9fd0483b18d19bda2a4987768045b551ca6
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2020-11-17 13:40:15 +0530

    Create variables_and_constants.go

---
+++ variables_and_constants.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int=5
+	var b float32=4.32
+	const pi float64=3.1415139475
+	var(
+		c=8
+		d=7
+	)
+	x,y:=14,15		
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(pi)
+	fmt.Println(c,",",d)
+	fmt.Println(x,",",y)
+}
