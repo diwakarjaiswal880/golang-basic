9c2c953d313f39583265add8a14e9589e20fb26f
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2020-11-19 17:56:28 +0530

    Taking input from user program

---
+++ input.go
@@ -0,0 +1,28 @@
+
+package main 
+  
+import "fmt"
+  
+// main function 
+func main() { 
+  
+    // Println function is used to 
+    // display output in the next line 
+    fmt.Println("Enter Your First Name: ") 
+  
+    // var then variable name then variable type 
+    var first string 
+  
+    // Taking input from user 
+    fmt.Scanln(&first) 
+    fmt.Println("Enter Second Last Name: ") 
+    var second string 
+    fmt.Scanln(&second) 
+  
+    // Print function is used to 
+    // display output in the same line 
+    fmt.Print("Your Full Name is: ") 
+  
+    // Addition of two string 
+    fmt.Print(first + " " + second) 
+} 
