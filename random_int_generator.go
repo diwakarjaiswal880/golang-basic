e2e922ecae22ffdf447bd209b2f1ea66e32f00ce
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2020-11-21 19:29:12 +0530

    Create random_int_generator.go

---
+++ random_int_generator.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	a1 := rand.Int()
+	a2 := rand.Int()
+	a3 := rand.Int()
+
+	fmt.Println("Random int Number 1: ", a1)
+	fmt.Println("Random int Number 2: ", a2)
+	fmt.Println("Random int Number 3: ", a3)
+}
